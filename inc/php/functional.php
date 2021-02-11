c676793001d990fbe56646b75987ad7dab22033b
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-02-11 11:01:47 +0300

    Maintenance: Code commenting improved

--- inc/php/functional.php
+++ inc/php/functional.php
@@ -51,7 +51,7 @@ function spacexchimp_p016_exec_footer_0() { spacexchimp_p016_prepare('footer_beg
 function spacexchimp_p016_exec_footer_1() { spacexchimp_p016_prepare('footer_end'); }
 
 /**
- * Inject the custom code into the website's frontend
+ * Inject the custom code into the website's frontend (head and footer sections)
  */
 add_action( 'wp_head', 'spacexchimp_p016_exec_head_0', 0 );
 add_action( 'wp_head', 'spacexchimp_p016_exec_head_1', 1000 );
