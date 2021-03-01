3d47b494f27b4656c7d31ea20f78e2a4adb5ee61
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-03-01 11:51:03 +0300

    Text correction: Code commenting

--- javascript-inserter.php
+++ javascript-inserter.php
@@ -68,7 +68,7 @@ spacexchimp_p016_define_constants( 'PREFIX', 'spacexchimp_p016' );
 spacexchimp_p016_define_constants( 'SETTINGS', 'spacexchimp_p016' );
 
 /**
- * A useful function that returns an array with the contents of plugin constants
+ * A useful function that returns an array with the contents of the plugin constants
  */
 function spacexchimp_p016_plugin() {
     $array = array(
